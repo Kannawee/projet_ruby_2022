26d8f4c7931482c6a294c359142732e1d132dc36
Yohan Centanni <thorbenliya@gmail.com>
2022-06-28 00:26:07 +0200

    minor changes about view template and controller refrigerateur

--- app/controllers/refrigerateurs_controller.rb
+++ app/controllers/refrigerateurs_controller.rb
@@ -3,5 +3,12 @@ class RefrigerateursController < ApplicationController
     def index
         @refrigerateurs = Refrigerateur.all
     end
+    def destroy
+        @refrigerateur.destroy
+    end
+    def new
+        @refrigerateur.new
+    end
+
 
 end
