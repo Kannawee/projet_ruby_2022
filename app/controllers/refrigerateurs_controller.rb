d7db0f9af9e58beed5d18c26c4835738c5a6ac37
Yohan Centanni <thorbenliya@gmail.com>
2022-06-28 22:42:43 +0200

    Add controller crud refrigerateur and views

--- app/controllers/refrigerateurs_controller.rb
+++ app/controllers/refrigerateurs_controller.rb
@@ -7,8 +7,12 @@ class RefrigerateursController < ApplicationController
         @refrigerateur.destroy
     end
     def new
-        @refrigerateur.new
+        @refrigerateur = Refrigerateur.new(params.permit(:name))
+        respond_to do |format|
+            if @refrigerateur.save
+              format.html { redirect_to '/', notice: "Le refrigerateur a bien été créé" }
+              format.json { render :show, status: :created, location: @refrigerateur }
+          end
     end
-
-
+ 
 end
