ac44b723bc044a69329f15b998378db12ee128d7
Yohan Centanni <thorbenliya@gmail.com>
2022-06-29 22:49:48 +0200

    add ingredients to refrigerateurs

---
+++ app/controllers/refrigerateur_ingredients_controller.rb
@@ -0,0 +1,21 @@
+class RefrigerateursController < ApplicationController
+
+    def index
+        @refrigerateurs = Refrigerateur.all
+        @ingredients = Ingredient.all
+    end
+
+    def destroy
+        @refrigerateur.destroy
+    end
+
+    def new
+        @refrigerateur = Refrigerateur.new(params.permit(:name))
+        respond_to do |format|
+            if @refrigerateur.save
+              format.html { redirect_to '/', notice: "Le refrigerateur a bien été créé" }
+              format.json { render :show, status: :created, location: @refrigerateur }
+            end
+        end
+    end
+end
--- app/controllers/refrigerateurs_controller.rb
+++ app/controllers/refrigerateurs_controller.rb
@@ -2,6 +2,7 @@ class RefrigerateursController < ApplicationController
 
     def index
         @refrigerateurs = Refrigerateur.all
+        @ingredients = Ingredient.all
     end
 
     def destroy
@@ -14,6 +15,19 @@ class RefrigerateursController < ApplicationController
             if @refrigerateur.save
               format.html { redirect_to '/', notice: "Le refrigerateur a bien été créé" }
               format.json { render :show, status: :created, location: @refrigerateur }
+            end
+        end
+    end
+
+    def add_ingredient
+        @refrigerateur_ingredient = Refrigerateuringredients.find_or_create_by(
+                            refrigerateur_id: params[:id], 
+                            ingredient_id: params[:ingredient_id]
+                             )
+        respond_to do |format|
+           if @refrigerateur_ingredient.save
+             format.html { redirect_to '/', notice: 'Ingredient a bien été ajouté au frigo' }
+             format.json { render :show, status: :created, location: @refrigerateur }
             end
         end
     end
